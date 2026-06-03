6b299337e95e90892efff005aed6f49e37eed219
STEAL <stealinp@gmail.com>
2026-06-03 21:51:28 +0800

    bogo

---
+++ bogosort.cpp
@@ -0,0 +1,41 @@
+%:include <iostream>
+%:include <ctime>
+
+void display(int arr<::>,int sizes)<%
+	std::cout<<"[";
+	for(int i=0;i<sizes-1;i++)<%
+		std::cout<<arr[i]<<",";
+	%>
+	std::cout<<arr[sizes-1]<<"]\n";
+%>
+
+void shuffle(int arr<::>,int sizes)<%
+	for(int i=0;i<10;i++)<%
+	int n=std::rand()%sizes;
+	int m=std::rand()%sizes;
+	int temp=arr<:n:>;
+	arr<:n:>=arr<:m:>;
+	arr<:m:>=temp;
+	%>
+%>
+
+bool check(int arr<::>,int sizes) <%
+	for(int i=1;i<sizes;i++) if(arr<:i:><arr<:i-1:>) return false;
+	return true;
+%>
+
+int main()<%
+	srand(time(NULL));
+	int sizes=10;
+	int arr<:sizes:>=<%9,3,1,6,10,7,4,2,5,8%>;
+	bool sorted=false;
+	int swap=0;
+	while(!sorted)<%
+		std::cout<<"Sort Attempt:"<<swap;
+		swap++;
+		shuffle(arr,sizes);
+		display(arr,sizes);
+		sorted=check(arr,sizes);
+		
+	%>
+%>
